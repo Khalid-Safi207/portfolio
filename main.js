b1245c3d8b091ac2e7a5628b32f1d5b82435935b
Khalid Safi <ksafi3198@gmail.com>
2024-07-19 21:28:42 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+let closeBtn = document.querySelector(".close");
+let menuBtn = document.querySelector(".menu");
+let ulEl = document.querySelector("ul");
+menuBtn.onclick = function () {
+  closeBtn.style.display = "block";
+  menuBtn.style.display = "none";
+  ulEl.style.left = "-10px";
+};
+closeBtn.onclick = function () {
+  ulEl.style.left = "-300%";
+  menuBtn.style.display = "flex";
+  closeBtn.style.display = "none";
+};
+
+let kalime = "F-u-l-l- -S-t-a-c-k- -D-e-v-e-l-o-p-e-r-.";
+let kalimeArr = kalime.split("-");
+const fullStackSpan = document.getElementById("full-stack")
+
+let index = 0;
+    
+setInterval(()=>{
+    if(index != 20){
+        fullStackSpan.innerText += `${kalimeArr[index]}`
+    index++;
+    }
+    else{
+        fullStackSpan.innerText = "";
+        index = 0;
+    }
+},240)
+
+
+let btn = document.getElementById("upBtn");
+
+window.onscroll = function(){
+    if(scrollY >= 400){
+        btn.style.display = "block";
+    }   
+    else{
+        btn.style.display = "none";
+    }
+}
+btn.onclick = function(){
+    scroll({
+        left: 0,
+        top: 0,
+        behavior: "smooth"
+    })
+}
